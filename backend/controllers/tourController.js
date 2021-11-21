349ce446b1ff14ec7ef57415b50eab8d4d7dc5cf
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2021-11-21 16:46:47 +0530

    backend done

---
+++ backend/controllers/tourController.js
@@ -0,0 +1,49 @@
+const Hotel = require("../models/hotelModel");
+
+exports.createHotel = async (req, res, next) => {
+  try {
+    const newHotel = await Hotel.create(req.body);
+    res.status(201).json({
+      status: "Success",
+      data: {
+        hotel: newHotel
+      }
+    })
+  } catch (err) {
+    res.status(500).json({
+      status: "Failed"
+    })
+  }
+}
+
+exports.getAllHotels = async (req, res, next) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(201).json({
+      status: "Success",
+      data: {
+        hotel: hotels
+      }
+    })
+  } catch (err) {
+    res.status(500).json({
+      status: "Failed"
+    })
+  }
+}
+
+exports.getHotel = async (req, res, next) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(201).json({
+      status: "Success",
+      data: {
+        hotel: hotel
+      }
+    })
+  } catch (err) {
+    res.status(500).json({
+      status: "Failed"
+    })
+  }
+}
---
+++ backend/utils/authUtil.js
@@ -0,0 +1,38 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel.js');
+const asyncHandler = require('express-async-handler');
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select('-password');
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error('401 Unauthorized - Token Failed');
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error('401 Unauthorized - Token Not Found');
+  }
+});
+
+const admin = asyncHandler(async (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error('401 Unauthorized - Authorized users only');
+  }
+});
+
+module.exports = { protect, admin };
