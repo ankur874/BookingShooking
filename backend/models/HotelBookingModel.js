ea5c4c8e7c366650996020c8e6e9ac0a0fa9915d
Ankur Attri <ankurattri874@gmail.com>
2021-11-24 12:11:24 +0530

    all

--- backend/models/HotelBookingModel.js
+++ backend/models/HotelBookingModel.js
@@ -1,22 +1,21 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const Hotel_Bookings = mongoose.Schema({
-    user_id:{ 
-       id: mongoose.Schema.Types.ObjectId,
-       required:true
-    },
-    // user_id
-    // number_of_tables
-    // types of tables
-    hotel_id:{
-        type:mongoose.Schema.Types.ObjectId,
-        required:true
-    },
-    booked_rooms:{
-        type:Number,
-        required:true
-    }
-})
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  // user_id
+  // number_of_tables
+  // types of tables
+  hotel_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+  },
+  booked_rooms: {
+    type: Number,
+    required: true,
+  },
+});
 
 module.exports = Hotel_Bookings;
