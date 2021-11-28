bc5957beed9f3f362c71b4dfa9347ed599982412
Ankur Attri <ankurattri874@gmail.com>
2021-11-28 11:16:03 +0530

    map added

--- frontend/src/Screens/Book.js
+++ frontend/src/Screens/Book.js
@@ -48,43 +48,41 @@ export default function Book() {
             {e.tableType == 1 && (
               <div
                 style={{ border: "1px solid #F3F4F6" }}
-                className="shadow-md flex flex-col justify-center items-center"
+                className="shadow-md "
               >
                 <img height="220" width="220" src={table_1} />
-                <div className="rounded-full bg-yellow-300"> </div>
+              
               </div>
             )}
             {e.tableType == 2 && (
               <div
                 style={{ border: "1px solid #F3F4F6" }}
-                className="shadow-md flex flex-col justify-center items-center"
+                className="shadow-md "
               >
                 <img height="220" width="220" src={table_2} />
-                <div className="rounded-full bg-yellow-300"> </div>
+                
               </div>
             )}
             {e.tableType == 3 && (
               <div
                 style={{ border: "1px solid #F3F4F6" }}
-                className=" shadow-md flex flex-col justify-center items-center "
+                className=" shadow-md "
               >
                 <img height="220" width="220" src={table_3} />
-                <div className="rounded-full bg-yellow-300"> </div>
+             
               </div>
             )}
             {e.tableType == 4 && (
-              <div 
-                
+              <div
                 style={{ border: "1px solid #F3F4F6" }}
-                className=" shadow-md flex flex-col justify-center items-center"
+                className=" shadow-md "
               >
                 <img height="220" width="220" src={table_4} />
-                <div className="rounded-full bg-yellow-300"> </div>
               </div>
             )}
           </div>
         ))}
-        {/* <button className='absolute bg-purple-800 text-white text-semibold rounded-sm shadow-sm hover:bg-purple-900  hover:-translate-y-1 transform p-2'>Book</button> */}
+        {/*   style={{ border: "1px solid #F3F4F6" }} <button className='absolute bg-purple-800 text-white text-semibold rounded-sm shadow-sm hover:bg-purple-900  hover:-translate-y-1 transform p-2'>Book</button> */}
       </div>
     );
   }
--- frontend/src/Screens/ExploreMap.js
+++ frontend/src/Screens/ExploreMap.js
@@ -1,6 +1,8 @@
 import ReactMapGL, { Marker } from "react-map-gl";
 import { useEffect, useState } from "react";
 import SideBar from "./SideBar";
+import { useDispatch, useSelector } from "react-redux";
+import { listRestaurants } from "../actions/restaurantActions";
 
 export default function ExploreMap() {
   const [modalStyle, setModalStyle] = useState(false);
@@ -16,77 +18,110 @@ export default function ExploreMap() {
     height: window.innerHeight,
     width: window.innerWidth,
   });
-  useEffect(() => {
-    return navigator.geolocation.getCurrentPosition(function (position) {
-      setMyLocation({
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-      });
+  const dispatch = useDispatch();
+  const data1 = useSelector((state) => state.restaurantList);
+  const { restaurants, loading1 } = data1;
+  if (restaurants.data != null) {
+    console.log(restaurants.data, "maodata");
+
+    // temp.push(restaurants.data[9]);
+    // temp.push(restaurants.data[8]);
+    // // temp.push(restaurants.data[2])
+    // temp.map((e) => console.log(e.coordinates[0], e.coordinates[1]));
+  }
+  navigator.geolocation.getCurrentPosition(function (position) {
+    setMyLocation({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
     });
   });
 
-  return (
-    <div className="grid grid-cols-8">
-      <div className="col-start-1 col-end-1">
-        <SideBar></SideBar>
+  useEffect(() => {
+    dispatch(listRestaurants());
+  }, [dispatch]);
+  if (restaurants.data == null) {
+    return (
+      <div className="h-screen flex flex-col items-center justify-center">
+        <img
+          height="200"
+          src="https://www.superiorlawncareusa.com/wp-content/uploads/2020/05/loading-gif-png-5.gif"
+          alt="some"
+        />
       </div>
+    );
+  } else {
+    return (
+      <div className="grid grid-cols-8">
+        <div className="col-start-1 col-end-1">
+          <SideBar></SideBar>
+        </div>
 
-      <div className="col-start-2 col-span-2">
-        <ReactMapGL
-          mapboxApiAccessToken={
-            "pk.eyJ1IjoiYW5rdXI4NzQiLCJhIjoiY2t3MXNkZmh5MWJ5bzJwbm9tZzBuM3Q4aCJ9._O6gqAm52rzHD1H_wMs_uA"
-          }
-          {...viewport}
-          onViewportChange={(viewport) => setViewport(viewport)}
-        >
-          {modalStyle ? (
-            <div
-              style={{
-                display: "flex",
-
-                flexDirection: "column",
-                width: "13rem",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-              className='bg-gray-700 p-2 text-gray-100 rounded-lg'
-            >
-              <img
-                className="object-cover w-24 h-24  rounded-full shadow"
-                src="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                alt="Person"
-              />
-              <div className="flex flex-col justify-center mt-2">
-                <p className="text-lg font-bold">Marta Clermont</p>
-                <p className="mb-4 text-xs text-gray-300">Design Team Lead</p>
-                <p className="text-sm tracking-wide text-gray-200">
-                  Secondary fermentation degrees plato units of bitterness, cask
-                  conditioned ale ibu real ale pint glass craft beer. krausen
-                  goblet grainy ibu.
-                </p>
-              </div>
-            </div>
-          ) : (
-            <div></div>
-          )}
-          <div
-          onMouseEnter={()=>{return setModalStyle(true)}}
-          onMouseLeave={()=>{return setModalStyle(false)}}
+        <div className="col-start-2 col-span-2">
+          <ReactMapGL
+            mapboxApiAccessToken={
+              "pk.eyJ1IjoiYW5rdXI4NzQiLCJhIjoiY2t3MXNkZmh5MWJ5bzJwbm9tZzBuM3Q4aCJ9._O6gqAm52rzHD1H_wMs_uA"
+            }
+            {...viewport}
+            onViewportChange={(viewport) => setViewport(viewport)}
           >
-            <Marker
-            className='transition-all duration-75 hover:shadow-md shadow-sm  transform  hover:scale-110 '
-              latitude={myLocation.latitude}
-              longitude={myLocation.longitude}
-              width={viewport.zoom * 5}
-              height={viewport.zoom * 5}
-            >
-              <i class="fas fa-map-marker"></i>
-            </Marker>
-          </div>
-        </ReactMapGL>
+            {
+              restaurants.data.map((e)=>  {return <>
+              {(
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    width: "13rem",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                  className="bg-gray-700 p-2 text-gray-100 rounded-lg"
+                >
+                  <img
+                    className="object-cover w-24 h-24  rounded-full shadow"
+                    src="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                    alt="Person"
+                  />
+                  <div className="flex flex-col justify-center mt-2">
+                    <p className="text-lg font-bold">{e.name}</p>
+                    <p className="mb-4 text-xs text-gray-300">
+                    {e.location}
+                    </p>
+                    <p className="text-sm tracking-wide text-gray-200">
+                      {e.description}
+                    </p>
+                  </div>
+                </div>
+              ) 
+                }
+              <div
+                onMouseEnter={() => {
+                   return setModalStyle(true);
+                }}
+                onMouseLeave={() => {
+                   return setModalStyle(false);
+                }}
+              >
+                
+                  <Marker
+                    className="transition-all duration-75 hover:shadow-md  transform  hover:scale-110 "
+                    latitude={e.coordinates[0]}
+                    longitude={e.coordinates[1]}
+                    width={viewport.zoom * 5}
+                    height={viewport.zoom * 5}
+                  >
+                    <i class="fas fa-map-marker"></i>
+                  </Marker>
+                )
+              </div>
+            </>})
+            }
+         
+          </ReactMapGL>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 // function modalOnHover(){
 //   return( );
